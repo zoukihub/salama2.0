d292910e17aa59a041e0675a198caed408ef5337
zoukihub <moraabochere@gmail.com>
2024-09-13 16:08:21 +0300

    chore Design product model

--- salama/products/models.py
+++ salama/products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('products:product_list_by_category', args=[self.slug])
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d'), blank=True
+
+    class Meta:
+        ordering = ['name']
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name 
+    
+    def get_absolute_url(self):
+        return reverse('products:product_detail', args=[self.id, self.slug])
+
 
 # Create your models here.

d2b2f9c1b0b9c863d8f148a552e414506d38680c
zoukihub <moraabochere@gmail.com>
2024-09-13 16:13:50 +0300

    chore design user profile model

--- salama/users/models.py
+++ salama/users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=225, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
 # Create your models here.

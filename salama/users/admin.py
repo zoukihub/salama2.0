2ab2343e56a8e6d156ddcd9aa5002990d30ea0f8
zoukihub <moraabochere@gmail.com>
2024-09-16 12:11:55 +0300

    feat(admin interface): add user admin functionality)

--- salama/users/admin.py
+++ salama/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserProfile
+
+admin.site.unregister(User)
+
+class UserProfileInline(admin.StackInline):
+    model = UserProfile
+    can_delete = False
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline, )
+
 
 # Register your models here.

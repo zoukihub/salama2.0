91ba1e307f1c23fe9ebd5baa5eed492eb4571207
zoukihub <moraabochere@gmail.com>
2024-09-17 21:51:19 +0300

    feat(cart): add forms file and forms class

---
+++ salama/cart/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.IntegerField(min_value=1, max_value=100, initial=1)
+    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+    

d8d3ef5d063827406c0661985e527f56d2391097
zoukihub <moraabochere@gmail.com>
2024-09-17 15:33:26 +0300

    feat(settings): use the console email backend

--- salama/salama/settings.py
+++ salama/salama/settings.py
@@ -144,6 +144,7 @@ SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 from django.contrib.messages import constants as messages
 

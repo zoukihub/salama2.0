891f380bcddba4494ad390e10b511684dce1baf5
zoukihub <moraabochere@gmail.com>
2024-09-17 19:02:11 +0300

    feat(products): implement urls to display products and categories

--- salama/products/urls.py
+++ salama/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),

6671bd096f1d13cd40c4c6beeb5c018921823e7c
zoukihub <moraabochere@gmail.com>
2024-09-18 22:03:17 +0300

    test(products): add unit test for product list view

--- salama/products/tests.py
+++ salama/products/tests.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.test import TestCase
 from.models import Category, Product
 
@@ -22,4 +23,24 @@ class ProductModelTest(TestCase):
     def test_product_stock(self):
         """"Test if the stock is correct"""
         self.assertEqual(self.product.stock, 50)
+
+class ProductListViewTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Medical Devices', slug='medical-devices')
+        self.product = Product.objects.create(
+            category=self.category,
+            name='Stethoscope',
+            slug='stethoscope',
+            description='High quality stethoscope.',
+            price=99.99,
+            stock=50,
+            available=True
+        )
+
+    def test_product_list_view(self):
+        """"Test if the product list view returns the correct status code and template"""
+        response=self.client.get(reverse('product_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'productsproduct/list.html')
+        self.assertIn(self.product, response.context['products'])
 # Create your tests here.

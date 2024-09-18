10b0590a97056d3a1b54f390a0f2a1e8990bc166
zoukihub <moraabochere@gmail.com>
2024-09-18 20:11:45 +0300

    feat(settings): handle static files and media files

--- salama/salama/settings.py
+++ salama/salama/settings.py
@@ -127,9 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

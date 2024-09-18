f5f63f7453106f505e65d57854c93d6742cb805c
zoukihub <moraabochere@gmail.com>
2024-09-18 18:16:01 +0300

    feat(orders): add urls for listing and viewing orders

--- salama/orders/urls.py
+++ salama/orders/urls.py
@@ -5,6 +5,7 @@ app_name = 'order'
 
 urlpatterns = [
     path('', views.order_list, name='order_list'),
+    path('<int:order_id>/', views.order_detail, name='order_detail'),
     path('payment/<int:order_id>/', views.payment, name='payment'),
     path('payment_success/', views.payment_success, name='payment_success'),
     path('payment_cancel/', views.payment_cancel, name='payment_cancel'),

a4e2e0e7ca4721f34f767b0da268482b2098bc38
zoukihub <moraabochere@gmail.com>
2024-09-19 22:41:23 +0300

    fix(urls): define url pattern for root url

--- salama/salama/urls.py
+++ salama/salama/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products import views as product_views
 
 
 urlpatterns = [
@@ -26,6 +27,7 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls')),
     path('products/', include('products.urls', namespace='products')),
+    path('', product_views.product_list, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

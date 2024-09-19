d15247358d7d73309d6eb328178713fba7fa311f
zoukihub <moraabochere@gmail.com>
2024-09-19 12:56:07 +0300

    feat(cart): add cart models

--- salama/cart/models.py
+++ salama/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Cart(models.Model):
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
 # Create your models here.

606f375bf866c9b91a97c55abd7e0ea41581f344
zoukihub <moraabochere@gmail.com>
2024-09-19 13:04:34 +0300

    fix(cart): add url pattern cart_add

--- salama/cart/urls.py
+++ salama/cart/urls.py
@@ -6,4 +6,5 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('add/', views.cart_add, name='cart_add'), 
 ]

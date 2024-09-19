7ff083dd05d1258d331b3b0f116dd7c758398fd5
zoukihub <moraabochere@gmail.com>
2024-09-19 13:39:53 +0300

    fix(cart): update view function

--- salama/cart/views.py
+++ salama/cart/views.py
@@ -25,6 +25,7 @@ def cart_detail(request):
     cart = Cart(request)
     return render(request, 'cart/cart_detail.html', {'cart': cart})
 
+from django.shortcuts import render, redirect
 from .models import Cart
 
 def cart_add(request):

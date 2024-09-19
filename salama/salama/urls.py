5d504dce27728836c747da36417af77c01010b1f
zoukihub <moraabochere@gmail.com>
2024-09-19 22:35:45 +0300

    fix(urls): removing duplicate product urls

--- salama/salama/urls.py
+++ salama/salama/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('products.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls')),
     path('products/', include('products.urls', namespace='products')),

bea5eb16f19d6ef8fd10527ed72ce75b301e97e6
zoukihub <moraabochere@gmail.com>
2024-09-19 09:34:34 +0300

    test(cart): add integration test for adding product to cart

--- salama/cart/tests.py
+++ salama/cart/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from products.models import Category, Product
+from cart.models import Cart
 
+class CartIntegrationTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Medical Devices', slug='medical-devices')
+        self.product = Product.objects.create(
+            category=self.category,
+            name='Stethoscope',
+            slug='stethoscope',
+            description='High quality stethoscope.',
+            price=99.99,
+            stock=50,
+            available=True
+        )
+    def test_add_product_to_cart(self):
+        """"Test adding a product to the cart"""
+        response = self.client.post(reverse('cart_add'), {'product_id' : self.product.id, 'quantity': 1})
+        self.assertEqual(response.status_code, 302)
+        cart = self.client.session['cart']
+        self.assertIn(str(self.product.id), cart)
+        self.assertEqual(cart[str(self.product.id)]['quantity'], 1)
 # Create your tests here.

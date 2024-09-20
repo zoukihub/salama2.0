4adfbc8c00370f2be8ffa8b7cfe9da5b833f3800
zoukihub <moraabochere@gmail.com>
2024-09-20 09:34:04 +0300

    fix(order): define the checkout url pattern

--- salama/orders/urls.py
+++ salama/orders/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
+    path('checkout/', views.checkout, name='checkout'),
     path('', views.order_list, name='order_list'),
     path('<int:order_id>/', views.order_detail, name='order_detail'),
     path('payment/<int:order_id>/', views.payment, name='payment'),

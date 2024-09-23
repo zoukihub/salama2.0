8a887ca8908958978c88201116230992f5a8e32a
zoukihub <moraabochere@gmail.com>
2024-09-23 21:59:55 +0300

    chore(account): update tasks.py

--- salama/account/tasks.py
+++ salama/account/tasks.py
@@ -2,6 +2,8 @@
 from celery import shared_task
 from django.core.mail import send_mail
 
+from salama.users.signals import User
+
 @shared_task
 def send_welcome_email(user_id):
     user = User.objects.get(id=user_id)
@@ -11,3 +13,5 @@ def send_welcome_email(user_id):
         'from@example.com',
         [user.email],
     )
+def send_order_confirmation(order_id):
+    pass

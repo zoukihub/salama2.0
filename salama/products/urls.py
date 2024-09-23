9e80f82aa26e00d387a8bb0be2c9daca40a82c66
zoukihub <moraabochere@gmail.com>
2024-09-23 21:50:22 +0300

    chore(products): update urls

--- salama/products/urls.py
+++ salama/products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
     path('search/', views.product_search, name='product_search'),
+    path('add/', views.add_product, name='add_product'),
 ]

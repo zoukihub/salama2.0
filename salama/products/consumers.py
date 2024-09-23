c9cd6bfdfad973798daa87c5bcc8f14a440cf650
zoukihub <moraabochere@gmail.com>
2024-09-23 20:51:33 +0300

    feat(products): create a consumer

---
+++ salama/products/consumers.py
@@ -0,0 +1,27 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = 'notifications'
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+    
+    async def receive(self, text_data):
+        pass
+
+    async def send_notification(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
